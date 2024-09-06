28428d639574a82939623cf5a00b09ceaaf77e24
David Alméciga <wdalmeciga@rcnradio.com.co>
2024-09-06 07:48:44 -0500

    rename databases

--- ad-manager/keystone.ts
+++ ad-manager/keystone.ts
@@ -21,7 +21,7 @@ export default withAuth(
       //   for more information on what database might be appropriate for you
       //   see https://keystonejs.com/docs/guides/choosing-a-database#title
       provider: 'sqlite',
-      url: 'file:./keystone.db',
+      url: 'file:./ad-manager.db',
     },
     lists,
     session,
