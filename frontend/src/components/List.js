46841533a43e636ebe2427da1faaafb3da010d99
David Alméciga <wdalmeciga@rcnradio.com.co>
2024-09-08 18:46:43 -0500

    include credentials

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -10,7 +10,8 @@ export default function List() {
       const res = await fetch('http://localhost:3000/api/graphql', {
         method: 'POST',
         body: JSON.stringify({"query":"query Query($where: PostWhereInput!) {\r\n  posts(where: $where) {\r\n    id\r\n    title\r\n    description\r\n    author {\r\n      name\r\n    }\r\n    video {\r\n      filename\r\n      url\r\n    }\r\n    tags {\r\n      name\r\n    }\r\n    placements {\r\n      assignment_id\r\n      id\r\n      status\r\n      provider_id\r\n      created_at\r\n      updated_at\r\n      error_was\r\n      provider_connection_strings\r\n    }\r\n  }\r\n}","variables":{"where":{"status":{"equals":"ASSIGNMENT"}}},"operationName":"Query"}),
-        headers: { 'Content-Type': 'application/json' }
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include'
       })
       const json = await res.json()
       setPosts(json.data.posts)
